96dd085712749488e59e829349d3de2c88168556
lizz <363664470@qq.com>
2017-02-07 22:06:23 +0800

    docs

--- docs/make.jl
+++ docs/make.jl
@@ -1,3 +1,10 @@
 using Documenter, CTMCEnsemble
 
 makedocs(modules=[CTMCEnsemble])
+
+deploydocs(
+    deps   = Deps.pip("mkdocs", "python-markdown-math"),
+    repo   = "github.com/innerlee/CTMCEnsemble.jl.git",
+    julia  = "0.5",
+    osname = "osx"
+)
