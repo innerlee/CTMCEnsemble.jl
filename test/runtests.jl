9f5574cbd8680f73636ecdf8a0627f64b85b33d8
lizz <363664470@qq.com>
2017-02-07 20:11:21 +0800

    rename

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,7 +3,7 @@ using Base.Test
 
 # write your own tests here
 
-@testset "love bufan" begin
+@testset "docs" begin
     readstring(`$(Base.julia_cmd()) -e 'cd("../docs");include("../docs/make.jl")'`)
     @test 1 == 1
 end
