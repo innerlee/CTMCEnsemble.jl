1751e0967385fb6cac9a2da86f7b184f1209f6f7
lizz <363664470@qq.com>
2017-02-08 18:56:03 +0800

    more methods

--- src/CTMCEnsemble.jl
+++ src/CTMCEnsemble.jl
@@ -1,25 +1,92 @@
 module CTMCEnsemble
 
 export
-    build,
-    stationdist
+    average,
+    product,
+    powermethod,
+    svdmethod,
+    ctmc
 
 """
-    build(preds, nclass=0; weights=nothing)
+    average(preds, weights=nothing; multiplicity=true)
+
+Compute the degree-weighted average.
+
+# Example
+
+```jldoctest
+julia> average([([0.5, 0.5], [1, 2]), ([0.5, 0.5], [2, 3])])
+3-element Array{Float64,1}:
+ 0.333333
+ 0.333333
+ 0.333333
+```
+"""
+function average(preds, weights=nothing; multiplicity=true)
+    nclass = maximum(maximum.(getindex.(preds, 2)))
+    weights == nothing && (weights = ones(length(preds)))
+    v = zeros(nclass)
+    m = zeros(nclass)
+    for ((pred, label), w) in zip(preds, weights)
+        v[label] .+= w .* pred
+        m[label] .+= w
+    end
+    if multiplicity
+        assert(all(m .> 0))
+        v ./= m
+    end
+    normalize!(v, 1)
+end
+
+"""
+    product(preds, weights=nothing; multiplicity=true)
+
+Compute the degree-weighted product.
+
+# Example
+
+```jldoctest
+julia> product([([0.5, 0.5], [1, 2]), ([0.5, 0.5], [2, 3])])
+3-element Array{Float64,1}:
+ 0.333333
+ 0.333333
+ 0.333333
+```
+"""
+function product(preds, weights=nothing; multiplicity=true)
+    nclass = maximum(maximum.(getindex.(preds, 2)))
+    weights == nothing && (weights = ones(length(preds)))
+    v = ones(nclass)
+    m = zeros(nclass)
+    for ((pred, label), w) in zip(preds, weights)
+        v[label] .*= w .* pred
+        m[label] .+= w
+    end
+    if multiplicity
+        assert(all(m .> 0))
+        v .^= (1 ./ m)
+    end
+    normalize!(v, 1)
+end
+
+assemble(p::Vector) = repmat(p, 1, length(p)) - eye(length(p))
+
+"""
+    build(preds, weights=nothing; nclass=0)
 
 Build the generator matrix.
 
 # Example
 
 ```jldoctest
-julia> build([([0.5, 0.5], [1, 2]), ([0.5, 0.5], [2, 3])])
+julia> CTMCEnsemble.build([([0.5, 0.5], [1, 2]), ([0.5, 0.5], [2, 3])])
 3×3 Array{Float64,2}:
  -0.5   0.5   0.0
   0.5  -1.0   0.5
   0.0   0.5  -0.5
 ```
 """
-function build(preds, nclass=0; weights=nothing)
+function build(preds, weights=nothing; nclass=0)
     nclass == 0 && (nclass = maximum(maximum.(getindex.(preds, 2))))
     weights == nothing && (weights = ones(length(preds)))
     G = zeros(nclass, nclass)
@@ -30,6 +97,52 @@ function build(preds, nclass=0; weights=nothing)
 end
 
 """
+    powermethod(preds, weights=nothing; maxiter=16)
+
+Compute stationary distribution by power method.
+
+# Example
+
+```jldoctest
+julia> powermethod([([0.5, 0.5], [1, 2]), ([0.5, 0.5], [2, 3])], maxiter=16)
+3-element Array{Float64,1}:
+ 0.333333
+ 0.333333
+ 0.333333
+"""
+function powermethod(preds, weights=nothing; maxiter=16)
+    A = build(preds, weights)
+    A .+= eye(A) .* length(preds)
+    v = average(preds, weights)
+
+    for i in 1:maxiter
+        v = normalize!(A * v, 1)
+    end
+
+    v / sum(v)
+end
+
+"""
+    svdmethod(preds, weights=nothing; maxiter=16)
+
+Compute stationary distribution by svd method.
+
+# Example
+
+```jldoctest
+julia> svdmethod([([0.5, 0.5], [1, 2]), ([0.5, 0.5], [2, 3])], maxiter=16)
+3-element Array{Float64,1}:
+ 0.333333
+ 0.333333
+ 0.333333
+"""
+function svdmethod(preds, weights=nothing;)
+    λ, ϕ = eig(build(preds, weights))
+    v = real(ϕ[:, indmin(abs(λ))])
+    v / sum(v)
+end
+
+"""
     stationdist(G)
 
 Compute stationary probability distribution given generator ``G``.
@@ -37,9 +150,9 @@ Compute stationary probability distribution given generator ``G``.
 # Example
 
 ```jldoctest
-julia> G = build([([0.5, 0.5], [1, 2]), ([0.5, 0.5], [2, 3])]);
+julia> G = CTMCEnsemble.build([([0.5, 0.5], [1, 2]), ([0.5, 0.5], [2, 3])]);
 
-julia> stationdist(G)
+julia> CTMCEnsemble.stationdist(G)
 3-element Array{Float64,1}:
  0.333333
  0.333333
@@ -54,6 +167,20 @@ function stationdist(G)
     v / sum(v)
 end
 
-assemble(p::Vector) = repmat(p, 1, length(p)) - eye(length(p))
+"""
+    ctmc(preds, weights=nothing)
+
+Compute stationary distribution by CTMC method.
+
+# Example
+
+```jldoctest
+julia> ctmc([([0.5, 0.5], [1, 2]), ([0.5, 0.5], [2, 3])], maxiter=16)
+3-element Array{Float64,1}:
+ 0.333333
+ 0.333333
+ 0.333333
+"""
+ctmc(preds, weights=nothing) = stationdist(build(preds, weights))
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,35 +2,54 @@ using CTMCEnsemble
 using Base.Test
 
 @testset "example" begin
-    G = build([([0.5, 0.5], [1, 2]), ([0.5, 0.5], [2, 3])])
-    @test stationdist(G) ≈ [1/3, 1/3, 1/3]
+    preds = [([0.5, 0.5], [1, 2]), ([0.5, 0.5], [2, 3])]
+    G = CTMCEnsemble.build(preds)
+    @test CTMCEnsemble.stationdist(G) ≈ [1/3, 1/3, 1/3]
+
+    @test [1/3, 1/3, 1/3] ≈ average(preds)
+    @test [1/4, 1/2, 1/4] ≈ average(preds, multiplicity=false)
+    @test [1/3, 1/3, 1/3] ≈ product(preds)
+    @test [2/5, 1/5, 2/5] ≈ product(preds, multiplicity=false)
+    @test [1/3, 1/3, 1/3] ≈ powermethod(preds)
+    @test [1/3, 1/3, 1/3] ≈ svdmethod(preds)
+    @test [1/3, 1/3, 1/3] ≈ ctmc(preds)
 end
 
-@testset "random cases" begin
+@testset "single" begin
     # single
-    for i = 1:10
+    for i = 1:5
         d = 1000
         p = normalize!(rand(d), 1)
-        G = build([(p, 1:d)])
-        @test stationdist(G) ≈ p
-    end
+        preds = [(p, 1:d)]
 
-    # multiple
-    for i = 1:10
+        @test p ≈ average(preds)
+        @test p ≈ average(preds, multiplicity=false)
+        @test p ≈ product(preds)
+        @test p ≈ product(preds, multiplicity=false)
+        @test p ≈ powermethod(preds)
+        @test p ≈ svdmethod(preds)
+        @test p ≈ ctmc(preds)
+    end
+end
+
+@testset "multiple" begin
+    newv() = normalize!(rand(300), 1)
+
+    for i = 1:5
         d = 600
-        newv() = normalize!(rand(300), 1)
-        G = build([
+        preds = [
             (newv(),     1:300),
             (newv(),   201:500),
             (newv(), ((401:700) - 1) % 600 + 1),
-        ])
-        λ, ϕ = eig(G)
-        v = real(ϕ[:, indmin(abs(λ))])
-        v = v / sum(v)
+        ]
 
-        @test stationdist(G) ≈ v
+        v_powermethod = powermethod(preds, maxiter=64)
+        v_svdmethod   = svdmethod(preds)
+        v_ctmc        = ctmc(preds)
+
+        @test v_svdmethod ≈ v_ctmc
+        @test_approx_eq_eps v_svdmethod v_powermethod 1/d
     end
-
 end
 
 @testset "love bufan" begin
